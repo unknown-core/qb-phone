9267b1690631655d2329c5205cc668e6f69dc6dd
AmirKasra_MomenKashani <71598934+AmirKasraKashani@users.noreply.github.com>
2022-05-28 10:26:17 +0430

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -272,7 +272,7 @@ local function OpenPhone()
     QBCore.Functions.TriggerCallback('qb-phone:server:HasPhone', function(HasPhone)
         if HasPhone then
             PhoneData.PlayerData = QBCore.Functions.GetPlayerData()
-            SetNuiFocus(true, false)
+            SetNuiFocus(false, true)
             SendNUIMessage({
                 action = "open",
                 Tweets = PhoneData.Tweets,
